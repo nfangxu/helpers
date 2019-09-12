6858b29a538fc127052223e7cb45796f3dc94aea
nfangxu <nfangxu@gmail.com>
2019-09-12 14:35:32 +0800

    add msectime()

---
+++ src/helpers.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!function_exists("msectime")) {
+    function msectime() {
+        list($msec, $sec) = explode(' ', microtime());
+        $msectime = (float)sprintf('%.0f', (floatval($msec) + floatval($sec)) * 1000);
+        return $msectime;
+    }
+}
